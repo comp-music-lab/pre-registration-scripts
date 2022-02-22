9f87bbef238a3d7e5d57c2024a76f12079a0002a
Hideo Daikoku <neildaikoku@gmail.com>
2022-02-22 10:14:17 +0900

    update with TOSTR

--- india-japan/equivalence-test.r
+++ india-japan/equivalence-test.r
@@ -1,30 +1,30 @@
 library(TOSTER)
 r <- 0.23
-d <- 2*r / (sqrt(1-r^2))
-r_u <- 0.3
-r_l <- -0.3
-d_u <- 2*r_u / (sqrt(1-r_u^2))
-d_l <- 2*r_l / (sqrt(1-r_l^2))
+n <- 60.0
+low_eqbound_r <- -0.3
+high_eqbound_r <- 0.3
 
-# TOSTmeta(ES = d, se=0.003, low_eqbound_d=d_l, high_eqbound_d=d_u, alpha=0.05)
+TOSTr(n, r, low_eqbound_r, high_eqbound_r, alpha=0.05, plot = TRUE, verbose = TRUE)
 # TOST results:
-# Z-value lower bound: 367.21 	p-value lower bound: 0.000
-# Z-value upper bound: -52.10 	p-value upper bound: 0.000
+# p-value lower bound: 0.00002
+# p-value upper bound: 0.285
 
-# Equivalence bounds (Cohen's d):
-# low eqbound: -0.629 
-# high eqbound: 0.629
+# Equivalence bounds (r):
+# low eqbound: -0.3 
+# high eqbound: 0.3
 
 # TOST confidence interval:
-# lower bound 90% CI: 0.468
-# upper bound 90% CI:  0.478
+# lower bound 90% CI: 0.016
+# upper bound 90% CI:  0.424
 
 # NHST confidence interval:
-# lower bound 95% CI: 0.467
-# upper bound 95% CI:  0.479
+# lower bound 95% CI: -0.025
+# upper bound 95% CI:  0.457
 
 # Equivalence Test Result:
-# The equivalence test was significant, Z = -52.100, p = 0.000, given equivalence bounds of -0.629 and 0.629 and an alpha of 0.05.
+# The equivalence test was non-significant, p = 0.285, given equivalence bounds of -0.300 and 0.300 and an alpha of 0.05.
 
 # Null Hypothesis Test Result:
-# The null hypothesis test was significant, Z = 157.557, p = 0.000, given an alpha of 0.05.
+# The null hypothesis test was non-significant, p = 0.0771, given an alpha of 0.05.
+
+# NHST: don't reject null significance hypothesis that the correlation is equal to 0 
